e481fa5f63f14ff1d701ede5f567d4f046392ad5
rabbitknight <mottchenz@gmail.com>
2018-12-15 20:12:24 +0800

    Fix Sub020

---
+++ app/src/main/java/net/rabbitknight/leetcode/core/Sub020.java
@@ -0,0 +1,57 @@
+package net.rabbitknight.leetcode.core;
+
+import net.rabbitknight.leetcode.PrintHelper;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ */
+public class Sub020 {
+    public boolean isValid(String s) {
+        if (null == s) {
+            return false;
+        }
+        if ("".equals(s)){
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.empty()||'(' != stack.pop()) {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.empty()||'[' != stack.pop()) {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.empty()||'{' != stack.pop()) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        Sub020 sub = new Sub020();
+        PrintHelper.println(sub.isValid("["));
+
+    }
+}
