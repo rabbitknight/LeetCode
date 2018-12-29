2f312428d26abd3b86483275712ce752fded6d71
rabbitknight <mottchenz@gmail.com>
2018-12-29 13:33:40 +0800

    Fix 027

---
+++ app/src/main/java/net/rabbitknight/leetcode/core/Sub025.java
@@ -0,0 +1,33 @@
+package net.rabbitknight.leetcode.core;
+
+import net.rabbitknight.leetcode.structure.ListNode;
+
+/**
+ * 给出一个链表，每 k 个节点一组进行翻转，并返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么将最后剩余节点保持原有顺序。
+ * <p>
+ * 示例 :
+ * <p>
+ * 给定这个链表：1->2->3->4->5
+ * <p>
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * <p>
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ * <p>
+ * 说明 :
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ */
+public class Sub025 {
+    // TODO: 2018/12/29 未解决
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode current = head;
+        return current;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
